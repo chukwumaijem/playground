fc4aec9bd0bd632526f337629c2a59927d86b211
Chukwuma Zikora <chukwuma.zikora@gmail.com>
2024-11-15 10:50:37 +0100

    read me, line clamp

--- thirdweb-app/src/app/components/ContractCard.tsx
+++ thirdweb-app/src/app/components/ContractCard.tsx
@@ -28,23 +28,19 @@ export function ContractCard(props: ContractCardProps) {
 
   return (
     <Link href={href}>
-      <Card className="group w-full h-full overflow-hidden">
+      <Card className="group h-full w-full overflow-hidden">
         <CardContent className="flex flex-col items-center justify-center !p-0">
           <MediaRenderer
             src={metadata?.image}
             client={client}
-            className="h-full w-full transform aspect-square transition-transform duration-300 group-hover:scale-[102%]"
+            className="aspect-square h-full w-full transform transition-transform duration-300 group-hover:scale-[102%]"
           />
         </CardContent>
 
-        <CardHeader className="flex h-full flex-col flex-grow justify-center border-t border-border !p-4">
-          <CardTitle className="line-clamp-1">
-            {metadata?.name}
-          </CardTitle>
+        <CardHeader className="flex h-full flex-grow flex-col justify-center border-t border-border !p-4">
+          <CardTitle className="line-clamp-1">{metadata?.name}</CardTitle>
           <CardDescription className="flex-grow">
-            <p className="line-clamp-2">
-              {metadata?.description}
-            </p>
+            <p className="line-clamp-2">{metadata?.description}</p>
           </CardDescription>
         </CardHeader>
       </Card>
